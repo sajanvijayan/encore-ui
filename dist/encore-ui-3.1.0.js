00894a2b0d2ac772094bdb06f3f0d965ea230b84
Ryan A. Johnson <ryan.johnson@rackspace.com>
2016-11-30 11:53:23 -0600

    docs(ghpages): release v3.1.0

--- dist/encore-ui-3.1.0.js
+++ dist/encore-ui-3.1.0.js
@@ -2,7 +2,7 @@
  * EncoreUI
  * https://github.com/rackerlabs/encore-ui
  *
- * Version: 3.1.0-0 - 2016-11-30
+ * Version: 3.1.0 - 2016-11-30
  * License: Apache-2.0
  */
 angular.module('encore.ui', [
